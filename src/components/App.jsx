7192016e353ff3bab41b9560577e38491715d9f9
Vladyslav <dyadyavadya308@gmail.com>
2023-05-22 09:49:37 +0300

    data downloaded

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,5 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import Profile from "./profile/profile";
+
+export default function App(params) {
+  <Profile/>
+}
